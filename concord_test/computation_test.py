1a53e5dd7f48699e24b25120496c275a1cf13ecf
agallego <gallego.alexx@gmail.com>
2015-07-28 14:02:55 -0400

    Proposed interface for tesing.
    Usage:
    
    ```
    import sys
    import concord
    from concord.computation import (
        Computation,
        Metadata
    )
    
    from concord_test.computation (
         serve_test_computation
    )
    
    import logging
    logging.basicConfig()
    logger = logging.getLogger(__name__)
    logger.setLevel(logging.DEBUG)
    
    class WordCounter(Computation):
        def init(self, ctx):
            self.__concord.publish("key", "value")
    
        def process_timer(self, ctx, key, time):
            ....
    
        def process_record(self, ctx, record):
            ....
    
        def metadata(self):
            #note that this gets called twice
    
            return Metadata(
                name='word-counter',
                istreams=['words'],
                ostreams=[])
    
    serve_test_computation(WordCounter())
    
    ```

--- concord_test/computation_test.py
+++ concord_test/computation_test.py
@@ -6,8 +6,7 @@ from kazoo.exceptions import (
     NoNodeError
 )
 from concord.computation import (
-    ComputationServiceWrapper,
-    Metadata,
+    serve_computation
 )
 
 import logging
@@ -17,119 +16,73 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-class ComputationTestServiceWrapper(ComputationServiceWrapper):
-    """
-    """
 
-    def __init__(self, handler, zookeeper_addr, test_id, node_id):
-        ComputationServiceWrapper.__init__(self, handler)
+class ConcordDecorator:
+    def __init__(self, computation_name, zookeeper_url, test_id, node_id):
+        assert(metadat != None)
+        assert(zookeepr_url != None)
+        assert(test_id != None)
+        assert(node_id != None)
+
+        self.computation_name = computation_name
+        self.zookeeper_url = zookeeper_url
         self.test_id = test_id
         self.node_id = node_id
-        self.computation_name = None
+        self.zk_path = '/bolt/testing/%s/%s/%s' % (
+            self.test_id, self.computation_name, self.node_id)
+        logger.info("Initialized decorator: ", self)
+        self.__connect_zookeeper()
+
+
+    def __connect_zookeeper(self):
         try:
-            logger.info('Attempting to connect to: %s' % zookeeper_addr)
-            self.zk = KazooClient(hosts=zookeeper_addr)
+            logger.info('Attempting to connect to: %s' % self.zookeeper_url)
+            self.zk = KazooClient(hosts=self.zookeeper_url)
             self.zk.start(timeout=10)
+            logger.info('Zookeeper: ', self.zk)
         except Exception as exception:
             logger.error('Failed to connect to zk')
             logger.fatal(exception)
 
+
+    def __str__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+
     def __del__(self):
         self.zk.stop()
 
-    def context_type(self):
-        return self
-
-    def zk_path(self):
-        # boltMetadata will be called before any method, assigning computation_name
-        assert(self.computation_name != None)
-        zk_path = '/bolt/testing/%s/%s/%s' % (self.test_id, self.computation_name, self.node_id)
-        return '%s/%s' % (base_path, self.computation_name)
-
-    def zk_record(self, data):
-        zk_path = self.zk_path()
+    def publish(self, key, data):
+        assert(key != None)
+        assert(data != None)
+        path = self.zk_path + "/%s" + key
+        logger.info("Creating data in: %s", path)
         try:
-            self.zk.set(zk_path, value=data)
-            logger.info('Writing init data to path %s' % zk_path)
-        except NoNodeError as exception:
-            logger.error('NoNodeError for path %s' % zk_path)
+            if not self.zk.exists(path):
+                logger.info("Path does not exist in zk, creating... %s", path)
+                self.zk.create(path, makepath=True)
+
+            self.zk.set(path, value=data)
+        except Exception as exception:
+            logger.error('Error setting data in ' % path)
             logger.fatal(exception)
 
-    def produce_record(self, stream, key, value):
-        self.zk_record('inside of produce_record!')
 
-    def set_timer(self, key, time):
-        self.zk_record('inside of set_timer!')
-
-    def init(self):
-        transaction = ComputationServiceWrapper.init(self)
-        self.zk_record('inside of init!')
-        return transaction
-
-    def boltProcessRecord(self, record):
-        transaction = ComputationServiceWrapper.boltProcessRecord(self, record)
-        self.zk_record('inside of processRecord!')
-        return transaction
-
-    def boltProcessTimer(self, key, time):
-        transaction = ComputationServiceWrapper.boltProcessTimer(self, key, time)
-        self.zk_record('inside of processTimer!')
-        return transaction
-
-    def boltMetadata(self):
-        metadata = ComputationServiceWrapper.boltMetadata(self)
-        logger.info('Inside bolt metadata!')
-        self.computation_name = metadata.name
-        try:
-            zk_path = '/bolt/testing/%s/%s' % (self.test_id, self.computation_name)
-            logger.info('Writing to path %s' % zk_path)
-            new_path = self.zk.create(zk_path, value=pickle.dumps(metadata))
-            logger.info('Created %s' % new_path)
-        except NodeExistsError as exception:
-            logger.error('NodeExistsError for path %s' % zk_path)
-            logger.fatal(exception)
-        return metadata
+def tryGetEnv(key):
+    try:
+        return os.environ[key]
+    except Exception as e:
+        logger.error('Error getting os.environ[%s]' % key)
+        logger.fatal(exception)
 
 def serve_test_computation(handler):
-    """Helper function. Parses environment variables and starts a thrift service
-        wrapping the user-defined computation.
-    :param handler: The user computation.
-    :type handler: Computation.
-    """
     logger.info("About to serve computation and service")
+    zookeeper_url = tryGetEnv('integration_test_zookeeper_url')
+    test_id = tryGetEnv('integration_test_id')
+    node_id = tryGetEnv('integration_test_node_id')
 
-    def address_str(address):
-        host, port = address.split(':')
-        return (host, int(port))
-
-    _, listen_port = address_str(
-        os.environ[kConcordEnvKeyClientListenAddr])
-    proxy_host, proxy_port = address_str(
-        os.environ[kConcordEnvKeyClientProxyAddr])
-
-    zookeeper_addr = os.environ['integration_test_zookeeper_addr']
-    test_id = os.environ['integration_test_id']
-    node_id = os.environ['integration_test_node_id']
-    comp = ComputationTestServiceWrapper(handler,
-                                         zookeeper_addr,
+    handler.__concord = ConcordDecorator(handler.metadata().name,
+                                         zookeeper_url,
                                          test_id,
                                          node_id)
-    processor = ComputationService.Processor(comp)
-    transport = TSocket.TServerSocket(port=listen_port)
-    server = TNonblockingServer.TNonblockingServer(processor, transport)
-
-    def thrift_service():
-        logger.info("Starting python service port: %d", listen_port)
-        server.serve()
-        logger.error("Exciting service")
-
-    try:
-        logger.info("registering with framework at: %s:%d",
-                            proxy_host, proxy_port)
-        comp.set_proxy_address(proxy_host, proxy_port)
-        thrift_service()
-    except Exception as exception:
-        logger.fatal(exception)
-        logger.error("Exception in python client")
-        server.stop()
-        raise exception
+    logger.info("Defering further init: concord.computation.serve_computation")
+    serve_computation(handler)
